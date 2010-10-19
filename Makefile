1d06c858a5893d4f1c0195364b9dcdf209ffb7b2
Steven Schronk <steven@schronk.com>
2010-10-19 16:12:39 -0500

    Added files for tokenizer module.

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ CFLAGS_REL = -Wall -ansi -O3
 CFLAGS_DBG = -Wall -pedantic -std=c99 -ansi -DDEBUG -g -O0 -ggdb3 -Wextra -Wno-missing-field-initializers
 CFLAGS_SML = -Wall -ansi -Os
 
-OBJS = jack.c error.c
-OUT = jack
+OBJS = jack.c error.c token.c
+OUT  = jack
 
 default: release
 
---
+++ token.h
@@ -0,0 +1,60 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+
+enum {
+	CLASS, METHOD, FUNCTION, CONSTRUCTOR, INT, BOOLEAN,
+	CHAR, VOID, VAR, STATIC, FIELD, LET, DO, IF, ELSE,
+	WHILE, RETURN, TRUE, FALSE, NULL, THIS
+} ttype;
+
+enum { KEYWORD, SYMBOL, IDENTIFIER, INT_CONST, STRING_CONST } token;
+
+/*
+* Look at source stream and determine if more tokens are available.
+* Returns > 0 if more are found and 0 if none found.
+*/
+int has_more_tokens();
+
+/*
+ * Move to next token in source stream.
+ * Should be called only if has_more_tokens is true.
+ */
+void advance(void);
+
+/*
+ * Returns type of current token.
+ */
+ttype token_type(void);
+
+/*
+ * Returns keyword of current token.
+ * Called only when token_type is KEYWORD.
+ */
+token keyword(void);
+
+/*
+ * Returns character which is the current token.
+ * Called only when token_type is a SYMBOL.
+ */
+char symbol();
+
+/*
+ * Returns identifier which is the current token.
+ * Called only when token_type is IDENTIFIER.
+ */
+char *identifier(char *str);
+
+/*
+ * Returns integer value of the current token.
+ * Called only when token_type is INT_CONST.
+ */
+int int_val();
+
+/*
+ * Returns a pointer to an array of char, without double quotes.
+ * Called only when token_type is STRING_CONST.
+ */
+char *string_val();
+
+#endif
+
