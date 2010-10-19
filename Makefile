6df121a590c263796c468170d58da7bc4b4c6386
Steven Schronk <steven@schronk.com>
2010-10-19 14:09:54 -0500

    File system setup.
    Compiler now opens input and output files.
    
    Added working Makefile and new Error codes.

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC=gcc
+
+CFLAGS_REL = -Wall -ansi -O3
+CFLAGS_DBG = -Wall -pedantic -std=c99 -ansi -DDEBUG -g -O0 -ggdb3 -Wextra -Wno-missing-field-initializers
+CFLAGS_SML = -Wall -ansi -Os
+
+OBJS = jack.c error.c
+OUT = jack
+
+default: release
+
+release: $(OBJS)
+	$(CC) $(CFLAGS_SML) $(LIBS) -o $(OUT) $^ 
+
+debug: $(OBJS)
+	$(CC) $(CFLAGS_DBG) $(LIBS) -o $(OUT) $^
+
+clean:
+	rm -f $(OUT)
--- jack.c
+++ jack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "error.h"
@@ -20,35 +21,62 @@ void settings_init(void)
 
 int main(int argc, char *argv[])
 {
-	char c;
-	FILE *fp;
+	int c, file_loc = 1;
+	char FilenameBuff[80];
+	char * pC;
+	FILE *fpSource, *fpDest;
 
 	if(argc < 2) { exit_error(1, "No Input Files."); usage(); }
 	/* TODO: future versions will accept more than one file */
 	if(argc > 3) { exit_error(2, "Too Many Files Listed."); usage(); }
 
-	while(-1 != (c = getopt(argc, argv,
-		"-v" /* verbose output to stdout */
-		"-t" /* print final hash table to stdout */
-		"-c" /* print code output to stdout - without comments */
-		"-C" /* print code output to stdout - with comments */
-		"-x" /* print commands to stdout */
-		"--help" /*print out usage statement */
-	))) {
+	/* test for switches passed - test for location of file in argv */
+	if(argv[1][0] == '-')
+	{
+		file_loc = 2;
+		while(-1 != (c = getopt(argc, argv,
+			"-v" /* verbose output to stdout */
+			"-t" /* print final hash table to stdout */
+			"-c" /* print code output to stdout - without comments */
+			"-C" /* print code output to stdout - with comments */
+			"-x" /* print commands to stdout */
+			"--help" /*print out usage statement */
+		))) {
 
-		switch (c)
-		{
-			case 'v':
-				settings.verbose = 1;
-				break;
-			case '-':
-				usage();
-				break;
+			switch (c)
+			{
+				case 'v':
+					settings.verbose = 1;
+					break;
+				case '-':
+					usage();
+					break;
+			}
 		}
 	}
 
-	if(( fp = fopen(argv[1], "r")) == NULL) {
+	if((fpSource = fopen(argv[file_loc], "r")) == NULL) {
 		exit_error(3, "Cannot Open Input (Source) File");
+	}
+
+	strcpy(FilenameBuff, argv[file_loc]);
+
+	/* verify filename extension */
+	if(strstr(argv[file_loc], "jack") == NULL)
+	{
+		exit_error(5, "Filename Extension Not Correct."); usage();
+	}
+
+	/* modify output filename and then open output file */
+	pC = strrchr(FilenameBuff, '.');
+	if( pC == NULL){ exit_error(5, "Input File Has No Extension"); usage(); }
+
+	pC++;
+	strcpy(pC, "xml"); /* buffer should contain output filename */
+
+	if((fpDest = fopen(FilenameBuff, "w+")) == NULL)
+	{
+		exit_error(6, "Cannot Open Output (Object) File");
 	}
 
 	return 0;
