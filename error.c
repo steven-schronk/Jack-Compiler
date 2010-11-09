137411ae5efe088af5e306818a4c9ae5784cee4a
Steven Schronk <steven@schronk.com>
2010-11-09 17:11:11 -0600

    Error messages now stop on correct token.
    Tab and space combinations in code causing arrow to bad token
    to print in incorrect location.
    
    Now following same space and tab configuration while printing arrow.

--- error.c
+++ error.c
@@ -23,8 +23,8 @@ void warning_error(const int err_num, const char *err_msg)
 void compiler_error(const int comp_num, const char *err_msg, char *pS, char *pC, char pT[])
 {
 	char *pTemp = pC;
+	char *pCount = pC;
 	int sp_count = 0; /* space count */
-	int tb_count = 0; /* tab count */
 	int line_num = 1;
 	int token_len = 0;
 
@@ -37,15 +37,13 @@ void compiler_error(const int comp_num, const char *err_msg, char *pS, char *pC,
 		pC--;
 	} while (strncmp(pT, pC, token_len) != 0);
 
+	pTemp = pC;
 	/* set pTemp to beginning of current line */
-	/* count tabs and spaces as you go */
 	while(pTemp > pS && *pTemp != '\n')
 	{
-		if(*pTemp == '\t') { tb_count++; }
-		else { sp_count++; }
+		sp_count++;
 		pTemp--;
 	}
-	sp_count = pC - pTemp;
 
 	/* count line number to beginning of file print for user later */
 	while(pS <= pC)
@@ -53,8 +51,9 @@ void compiler_error(const int comp_num, const char *err_msg, char *pS, char *pC,
 		if(*pC == '\n') { line_num++; }
 		pC--;
 	}
+	pTemp++; /* move past newline */
+	pCount = pTemp;
 
-	pTemp++;
 	/* print last line to stderr */
 	while((*pTemp != '\n') && (*pTemp != '\0'))
 	{
@@ -63,10 +62,22 @@ void compiler_error(const int comp_num, const char *err_msg, char *pS, char *pC,
 	}
 	fprintf(stderr, "\n");
 
+	sp_count--; /* off by one because of arrow printed at end of line*/
+
 	/* print arrow pointing to token compiler cannot comprehend */
-	while(sp_count > 1) { fprintf(stderr, " ");  sp_count--; }
-	while(tb_count > 0)	{ fprintf(stderr, "\t"); tb_count--; }
-	fprintf(stderr, "^\n");
+	while(sp_count >= 0) {
+		if(sp_count == 0) { fprintf(stderr, "^\n");
+		} else {
+			if(*pCount == '\t')
+			{
+				fprintf(stderr, "\t");
+			} else  {
+				fprintf(stderr, " ");
+			}
+		}
+		sp_count--;
+		pCount++;
+	}
 
 	fprintf(stderr, "ERROR %d: %s On Line %d\n\n", comp_num, err_msg, line_num);
 	exit(EXIT_SUCCESS);
@@ -74,5 +85,5 @@ void compiler_error(const int comp_num, const char *err_msg, char *pS, char *pC,
 
 void line_notification(const int line_num)
 {
-	fprintf(stderr, "ERROR FOUND ON LINE: %d\n", line_num);
+	fprintf(stderr, "ERROR FOUND ON LINE: %d.\n", line_num);
 }
